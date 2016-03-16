92cbf8526c171e6b9ee9c8c5808dce1cb5917b74
Adson <shinadson@gmail.com>
2016-03-16 18:41:57 -0300

    Entering into Angular?

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 (function() {
   var app = angular.module('Descript', []);
 
-  app.controller('DescriptController', function(){
-    this.product = description;
+  app.controller('TabController', function(){
+    this.tab=1;
+    this.setTab = function(setTab)
+    {
+      this.tab=setTab;
+    };
+    this.isSet = function(checktab)
+    {
+      return this.tab === checktab;
+    };
   });
 
   var description = [
