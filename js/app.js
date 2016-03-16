716d17cc5aad8bed75e8cafbf821f5c558a6b7c4
Adson <shinadson@gmail.com>
2016-03-16 16:33:14 -0300

    Just Site Things

---
+++ js/app.js
@@ -0,0 +1,19 @@
+(function() {
+  var app = angular.module('Descript', []);
+
+  app.controller('DescriptController', function(){
+    this.product = description;
+  });
+
+  var description = [
+        {
+
+        },
+        {
+
+        },
+        {
+
+        }
+    ];
+})();
---
+++ js/jsbutton.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$("#buttonclick").click(function(){
+    $("#descricao").show();
+});
+$(document).ready(function(){
+    $("#descricao").hide();     
+});
+
+var data={
+    "nomes" : [ 
+    {
+        "nome": dsfsf,
+        "sadads": dasds
+    },
+    {
+        "nome": dsfsf,
+        "sadads": dasds
+    }]
+};
