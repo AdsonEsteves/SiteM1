8f4ac1f6a83a0da6af68fecc3893beb3c434995b
Adson Marques da Silva Esteves <3798925@univali.br>
2016-03-16 21:45:35 -0300

    Arrumando Habilidades

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 (function() {
   var app = angular.module('Descript', []);
-
+   
+    app.controller('DescriptController', function(){
+    this.texto = description;
+    });
+    
   app.controller('TabController', function(){
     this.tab=1;
     this.setTab = function(setTab)
@@ -13,15 +17,15 @@
     };
   });
 
-  var description = [
-        {
-
-        },
-        {
-
-        },
-        {
-
-        }
-    ];
+  var description = {
+            C : "Algum texto sobre C++",
+            JAVA : "Algum texto sobre JAVA",
+            PHP : "Algum texto sobre PHP",
+            LUA: "Algum texto sobre LUA",
+            GO : "Algum texto sobre GO",
+            Javascript : "Algum texto sobre Javascript",
+            HTMLCSS : "Algum texto sobre HTML/CSS",
+            Corel: "Algum Texto sobre Corel",
+            Photoshop: "Algum texto sobre PS"
+  };
 })();
